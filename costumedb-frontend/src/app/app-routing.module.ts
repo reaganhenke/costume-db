1acce5ddd09f455fca8e89f0c524d0d7f3d09270
Reagan Henke <reaganhenke@gmail.com>
2021-09-19 14:05:00 -0400

    add placeholder pages and routes

--- costumedb-frontend/src/app/app-routing.module.ts
+++ costumedb-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GroupSearchComponent } from './group-search/group-search.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SharePageComponent } from './share-page/share-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'groupsearch', component: GroupSearchComponent },
+  { path: 'share', component: SharePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- costumedb-frontend/src/app/app.module.ts
+++ costumedb-frontend/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { extModules } from 'src/environments';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GroupSearchComponent } from './group-search/group-search.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SharePageComponent } from './share-page/share-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GroupSearchComponent,
+    HomePageComponent,
+    SharePageComponent
   ],
   imports: [
     BrowserModule,
