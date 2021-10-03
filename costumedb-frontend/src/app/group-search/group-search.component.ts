46004508cb22dcb90106abf15dfde2deec17bccc
Reagan Henke <reaganhenke@gmail.com>
2021-10-03 16:26:46 -0400

    update group search costumes

--- costumedb-frontend/src/app/group-search/group-search.component.ts
+++ costumedb-frontend/src/app/group-search/group-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from 'src/state/appState';
@@ -13,15 +13,16 @@ import { CostumeResponse } from '../models/costume-response.model';
 })
 export class GroupSearchComponent implements OnDestroy {
   groupSize = 0;
-  groupSizeForm = new FormControl('', [Validators.required, Validators.pattern(/[0-9]/), Validators.maxLength(1)]);
+  groupSizeForm = new FormControl('', [Validators.required, Validators.max(9), Validators.min(1)]);
   group: FormGroup [] = [];
+  removing = false;
 
   loading$: Observable<boolean>;
   loaded$: Observable<boolean>;
   results$: Observable<CostumeResponse>;
   error$: Observable<string | null>;
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<AppState>, private fb: FormBuilder) {
     this.loading$ = this.store.select(state => state.groupSearch.loading);
     this.loaded$ = this.store.select(state => state.groupSearch.loaded);
     this.results$ = this.store.select(state => state.groupSearch.results);
@@ -38,30 +39,62 @@ export class GroupSearchComponent implements OnDestroy {
   }
 
   getNewFormGroup() {
-    return new FormGroup({
-      'gender': new FormControl('any'),
-      'hair': new FormControl('any')
+    return this.fb.group({
+      showAdvanced: new FormControl(false),
+      gender: new FormControl(''),
+      hair: new FormControl(''),
+      bodyType: new FormControl(''),
+      ethnicity: new FormControl('')
     });
   }
 
   addCostume() {
     this.group.push(this.getNewFormGroup());
+    this.groupSize++;
   }
 
   removeCostume(i: number) {
-    this.group.splice(i, 1);
+    if (!this.removing){
+      this.removing = true;
+
+      let domElement = document.getElementById("costume-" + i);
+      let flag = true;
+      domElement?.addEventListener("transitionend", () => {
+        if (flag) {
+          this.group.splice(i, 1);
+          this.groupSize--;
+        }
+        flag = false;
+        this.removing = false;
+      })
+      domElement?.classList.add('remove');
+    }
   }
 
   submit() {
     let values: String[] = [];
     this.group.forEach(costume => {
+      costume.removeControl('showAdvanced');
       values.push(costume.value)
     });
     this.store.dispatch(loadGroupCostumes(values));
   }
 
+  reset() {
+    for(let costumeForm of this.group) {
+      costumeForm.reset({
+        'showAdvanced': false,
+        'gender': '',
+        'hair': '',
+        'bodyType': '',
+        'ethnicity': ''
+      });
+    }
+  }
+
   retry() {
     this.groupSize = 0;
+    this.group = [];
     this.store.dispatch(clearGroupCostumesSearch());
   }
 
